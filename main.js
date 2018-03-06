ffbb79742d066f6eced355afa922f3eb6c28f372
Kay Mok <kaymok3@gmail.com>
2018-03-06 16:27:43 -0500

    Add prompt for challenge #1

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+Using a for loop, compute the sum of the numbers in the array named prices;
+storing the result in the sum variable.
+*/
+
+var prices = [33, 26, 99, 120, 12, 45];
+var sum = 0;
+
+// Your code here
+
+console.log("The sum is: ", sum);
