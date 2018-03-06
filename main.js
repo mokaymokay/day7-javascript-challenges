6b2ec3b17af2e7d658b97e25f4e1f8cdc44b186c
Kay Mok <kaymok3@gmail.com>
2018-03-06 16:29:38 -0500

    Add answers for challenge #1

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 /*
+1 - A running total.
+
 Using a for loop, compute the sum of the numbers in the array named prices;
 storing the result in the sum variable.
 */
@@ -6,6 +8,8 @@ storing the result in the sum variable.
 var prices = [33, 26, 99, 120, 12, 45];
 var sum = 0;
 
-// Your code here
+for (let i = 0; i < prices.length; i++) {
+  sum += prices[i];
+}
 
 console.log("The sum is: ", sum);
